220eb187f5272e3c169b5e298c012f9e24425443
Виктор Тузовский <aletissarichard@gmail.com>
2022-09-04 23:59:58 +0300

    Created follow model

---
+++ backend/users/models.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Follow(models.Model):
+	user = models.ForeignKey(
+		User,
+		on_delete=models.CASCADE,
+		related_name='follower',
+		verbose_name='Подписчик',
+	)
+	author = models.ForeignKey(
+		User,
+		on_delete=models.CASCADE,
+		related_name='following',
+		verbose_name='Автор',
+	)
+
+	class Meta:
+		ordering = ['-id']
+		verbose_name = 'Подписка'
+		constraints = [
+			models.UniqueConstraint(
+				fields=['user', 'author'],
+				name='follow unique',
+			)
+		]
