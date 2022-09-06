3974a0afc692a528c2fe65f145a7d9541ae86541
Виктор Тузовский <aletissarichard@gmail.com>
2022-09-07 01:02:42 +0300

    Added djoser auth urls

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('djoser.urls.authtoken')),
     path('api/', include('users.urls')),
+    path('api/', include('recipes.urls')),
 ]
