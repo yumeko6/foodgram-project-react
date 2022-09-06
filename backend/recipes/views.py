98ff01fe7865b9e1124472231ed1a66363f28cfb
Виктор Тузовский <aletissarichard@gmail.com>
2022-09-07 01:05:49 +0300

    Added recipes views

---
+++ backend/recipes/views.py
@@ -0,0 +1,106 @@
+from django.db.models import Sum
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import permissions, status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from users.paginators import CustomPagination
+from .filters import IngredientsFilter, RecipeFilter
+from .mixins import RetrieveAndListViewSet
+from .models import (
+	Favorite, Ingredient, Recipe, RecipeIngredient, ShoppingList, Tag,
+)
+from .permissions import IsAuthorOrAdmin
+from .serializers import (
+	AddRecipeSerializer, FavouriteSerializer, IngredientsSerializer,
+	ShoppingListSerializer, ShowRecipeFullSerializer, TagsSerializer,
+)
+
+
+class IngredientsViewSet(RetrieveAndListViewSet):
+	queryset = Ingredient.objects.all()
+	serializer_class = IngredientsSerializer
+	permission_classes = [permissions.AllowAny]
+	filter_backends = [DjangoFilterBackend]
+	filterset_class = IngredientsFilter
+	pagination_class = None
+
+
+class TagsViewSet(RetrieveAndListViewSet):
+	queryset = Tag.objects.all()
+	serializer_class = TagsSerializer
+	permission_classes = [permissions.AllowAny]
+	pagination_class = None
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+	queryset = Recipe.objects.all()
+	serializer_class = ShowRecipeFullSerializer
+	permission_classes = [IsAuthorOrAdmin]
+	filter_backends = [DjangoFilterBackend]
+	filterset_class = RecipeFilter
+	pagination_class = CustomPagination
+
+	def get_serializer_class(self):
+		if self.request.method == 'GET':
+			return ShowRecipeFullSerializer
+		return AddRecipeSerializer
+
+	@action(detail=True, permission_classes=[IsAuthorOrAdmin], methods=['post'])
+	def favorite(self, request, pk):
+		data = {'user': request.user.id, 'recipe': pk}
+		serializer = FavouriteSerializer(
+			data=data, context={'request': request}
+		)
+		serializer.is_valid(raise_exception=True)
+		serializer.save()
+		return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+	@favorite.mapping.delete
+	def delete_favorite(self, request, pk):
+		user = request.user
+		recipe = get_object_or_404(Recipe, id=pk)
+		favorite = get_object_or_404(Favorite, user=user, recipe=recipe)
+		favorite.delete()
+		return Response(status=status.HTTP_204_NO_CONTENT)
+
+	@action(detail=True, permission_classes=[IsAuthorOrAdmin], methods=['post'])
+	def shopping_cart(self, request, pk):
+		data = {'user': request.user.id, 'recipe': pk}
+		serializer = ShoppingListSerializer(
+			data=data, context={'request': request}
+		)
+		serializer.is_valid(raise_exception=True)
+		serializer.save()
+		return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+	@shopping_cart.mapping.delete
+	def delete_shopping_cart(self, request, pk):
+		user = request.user
+		recipe = get_object_or_404(Recipe, id=pk)
+		shopping_list = get_object_or_404(
+			ShoppingList, user=user, recipe=recipe
+		)
+		shopping_list.delete()
+		return Response(status=status.HTTP_204_NO_CONTENT)
+
+	@action(detail=False, permission_classes=[permissions.IsAuthenticated])
+	def download_shopping_cart(self, request):
+		user_shopping_list = RecipeIngredient.objects.filter(
+			recipe__shoppinglist__user=request.user).values_list(
+			'ingredient__name', 'amount', 'ingredient__measurement_unit')
+		all_count_ingredients = user_shopping_list.values(
+			'ingredient__name', 'ingredient__measurement_unit').annotate(
+			total=Sum('amount')).order_by('-total')
+		ingredients_list = []
+		for ingredient in all_count_ingredients:
+			ingredients_list.append(
+				f'{ingredient["ingredient__name"]} - '
+				f'{ingredient["total"]} '
+				f'{ingredient["ingredient__measurement_unit"]} \n'
+			)
+		response = HttpResponse(ingredients_list, 'Content-Type: text/plain')
+		response['Content-Disposition'] = 'attachment; filename="to_buy.txt"'
+		return response
