5e7a6e568c0cdd1f56e23815c2588329137c7fb1
Виктор Тузовский <aletissarichard@gmail.com>
2022-09-07 23:43:34 +0300

    Revert "Minor changes"
    This reverts commit ff71cb69434d5ccb6b74b1c2881be7c5845c93fc.

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -3,6 +3,7 @@ from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
+from recipes.models import Recipe
 from .models import Follow
 
 User = get_user_model()
@@ -45,37 +46,35 @@ class CurrentUserSerializer(UserSerializer):
 
 
 class FollowSerializer(serializers.ModelSerializer):
-    queryset = User.objects.all()
-    user = serializers.PrimaryKeyRelatedField(queryset=queryset)
-    author = serializers.PrimaryKeyRelatedField(queryset=queryset)
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    following = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
 
     class Meta:
         model = Follow
-        fields = ('user', 'author')
+        fields = ('user', 'following')
 
     def validate(self, data):
-        request = self.context.get('request')
-        author_id = data['author'].id
-        follow_exists = Follow.objects.filter(
-            user=request.user, author__id=author_id).exists()
-        if request.method == 'GET':
-            if request.user.id == author_id:
-                raise serializers.ValidationError(
-                    {'error': 'Нельзя подписаться на самого себя!'}
-                )
-            if follow_exists:
-                raise serializers.ValidationError(
-                    {'error': 'Вы уже подписаны на этого автора!'}
-                )
+        user = self.context.get('request').user
+        following_id = data['following'].id
+        if Follow.objects.filter(
+                user=user, following__id=following_id).exists():
+            raise serializers.ValidationError(
+                'Вы уже подписаны на этого пользователя!'
+            )
+        if user.id == following_id:
+            raise serializers.ValidationError(
+                'Нельзя подписаться на самого себя!'
+            )
         return data
 
 
+class FollowingRecipesSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = ('id', 'name', 'image', 'cooking_time')
+
+
 class FollowerSerializer(serializers.ModelSerializer):
-    id = serializers.ReadOnlyField(source='author.id')
-    email = serializers.ReadOnlyField(source='author.email')
-    username = serializers.ReadOnlyField(source='author.username')
-    first_name = serializers.ReadOnlyField(source='author.first_name')
-    last_name = serializers.ReadOnlyField(source='author.last_name')
     is_subscribed = serializers.SerializerMethodField()
     recipes = serializers.SerializerMethodField()
     recipes_count = serializers.SerializerMethodField()
@@ -84,9 +83,24 @@ class FollowerSerializer(serializers.ModelSerializer):
         model = User
         fields = ('email', 'id', 'username', 'first_name', 'last_name',
                   'is_subscribed', 'recipes', 'recipes_count')
+        read_only_fields = fields
 
     def get_is_subscribed(self, obj):
-        request = self.context.get('request')
-        if request.user.is_anonymous:
+        user = self.context['request'].user
+        if user.is_anonymous:
             return False
-        return Follow.objects.filter(user=obj.user, author=obj.author).exists()
+        return obj.follower.filter(user=obj, following=user).exists()
+
+    def get_recipes(self, obj):
+        request = self.context.get('request')
+        recipes_limit = request.query_params.get('recipes_limit')
+        if recipes_limit is not None:
+            recipes = obj.recipes.all()[:(int(recipes_limit))]
+        else:
+            recipes = obj.recipes.all()
+        context = {'request': request}
+        return FollowingRecipesSerializers(
+            recipes, many=True, context=context).data
+
+    def get_recipes_count(self, obj):
+        return obj.recipes.count()
